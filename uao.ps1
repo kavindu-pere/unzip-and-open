baed4819a88671dea7d1eca841421d1b86be2206
Kavindu Perera <59706267+icodelazy@users.noreply.github.com>
2023-07-02 00:07:55 +0530

    Create uao.ps1

---
+++ uao.ps1
@@ -0,0 +1,21 @@
+#PowerShell
+
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$ZipFile
+)
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+# Remove the ".zip" extension from the input file name
+$projectFolderName = $zipPath -replace '\.zip$'
+
+# Get the absolute path of the .zip file
+$zipPath = Convert-Path $ZipFile
+
+# Unzip the .zip file
+[System.IO.Compression.ZipFile]::ExtractToDirectory($zipPath, $pwd)
+
+# Execute the command
+$command = "idea $projectFolderName"
+Invoke-Expression -Command $command
